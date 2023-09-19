88237c0119805cf497c26d1d7d0dc6b7c7c1ef0c
cristian1016 <cpulgarinsalgado@gmail.com>
2023-09-19 10:31:30 -0500

    Navbar and Header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,15 @@ import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <>
-      <div>
-        <ul  className='navbar'>
-          <li><NavLink to='/'>Home</NavLink></li>
-          <li><NavLink to='/projects'>Projects</NavLink></li>
-          <li><NavLink to='/contact'>Contact</NavLink></li>
-        </ul>
-      </div>
-    </>
-)
-}
+    <div>
+      <ul className='navbar'>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/projects'>Projects</NavLink></li>
+        <li><NavLink to='/contact'>Contact</NavLink></li>
+      </ul>
+    </div>
+  );
+};
+
+
+
