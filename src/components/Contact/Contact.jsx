91a3a72179be65a4186c7aab7faf556738aa05a1
cristian1016 <cpulgarinsalgado@gmail.com>
2023-09-19 08:50:41 -0500

    add header in the contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,46 +1,50 @@
 import React from 'react'
 import './Contact.css'
+import { Header } from '../Header/Header'
 
 
 export default function Contact() {
     return (
-        <section>
-            <div className='form'>
-                <div className='registro_center'>
-                    <div className='registro'>
-                        <div id="registro_titu">
-                            <h3>CONTACT ME</h3>
+        <div className='contact'>
+        <Header/>
+            <section>
+                <div className='form'>
+                    <div className='registro_center'>
+                        <div className='registro'>
+                            <div id="registro_titu">
+                                <h3>CONTACT ME</h3>
+                            </div>
+
+                            <div class="digitar">
+                                <input name="nombres" type="text" required />
+                                <label for="digitar">Nombre</label>
+                            </div>
+
+                            <div class="digitar">
+
+                                <input name="apellidos" type="text" required />
+                                <label for="digitar">Apellidos</label>
+                            </div>
+
+                            <div class="digitar">
+                                <input name="correo" type="email" required />
+                                <label for="digitar">Email</label>
+                            </div>
+
+                            <div class="digitar">
+                                <input name="telefono" type="tel" required />
+                                <label>Telefono</label>
+                            </div>
                         </div>
 
-                        <div class="digitar">
-                            <input name="nombres" type="text" required />
-                            <label for="digitar">Nombre</label>
-                        </div>
-
-                        <div class="digitar">
-
-                            <input name="apellidos" type="text" required />
-                            <label for="digitar">Apellidos</label>
-                        </div>
-
-                        <div class="digitar">
-                            <input name="correo" type="email" required />
-                            <label for="digitar">Email</label>
-                        </div>
-
-                        <div class="digitar">
-                            <input name="telefono" type="tel" required />
-                            <label>Telefono</label>
-                        </div>
-                    </div>
-
-                    <div class="buttom">
-                        <div id="input_registrar">
-                            <input id='registras_boton' type="submit" value="Contact me" />
+                        <div class="buttom">
+                            <div id="input_registrar">
+                                <input id='registras_boton' type="submit" value="Contact me" />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     )
 }
